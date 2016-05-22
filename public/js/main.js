192f15b85338a116456e37ecd79c5c8bafdf34a8
Michael Duran <mad2116@gmail.com>
2016-05-22 15:07:31 -0700

    add basic styling and image assets

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,14 @@
 console.log("main.js loaded!");
+
+$(function() {
+
+  var image = "assets/flipbook-images/rhome_001.png";
+
+});
+
+function fileNameSerializer(prefix, zeros, index, fileType) {
+  var indexString;
+  var index = String(index);
+  indexString = index.length >= zeros ? index : String(0).repeat(zeros - index.length) + index;
+  return prefix + indexString + "." + fileType;
+}
