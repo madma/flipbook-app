d666c398d76ba940f6a6f62ffcbc0932264d2764
Michael Duran <mad2116@gmail.com>
2016-05-23 20:09:09 -0700

    add working version of code that loads an image from a sequence based on mouse location in img element

--- public/js/main.js
+++ public/js/main.js
@@ -2,9 +2,30 @@ console.log("main.js loaded!");
 
 $(function() {
 
-  var image = "assets/flipbook-images/rhome_001.png";
+  var prefix = "assets/flipbook-images/rhome_";
+  var zeros = 3;
+  var fileType = "png";
+  var imgSeqLength = 104;
+
+  // get x position of mouse within img#current-page
+
+  var $currentPage = $("#current-page");
+
+  $currentPage.on("mousemove", function(event) {
+    var $this = $(this);
+    var x = getXOnElement($this, event.pageX);
+    var i = Math.ceil((x/$this.width())*imgSeqLength);
+    var src = fileNameSerializer(prefix, zeros, i, fileType);
+    console.log("The x position on this element is: " + x);
+    console.log("The image index i is: " + i);
+    $this.attr("src", src);
+  });
 
 });
+
+function getXOnElement(element, pageX) {
+  return Math.ceil(Math.abs(pageX - element.offset().left));
+}
 
 function fileNameSerializer(prefix, zeros, index, fileType) {
   var indexString;
@@ -14,5 +35,8 @@ function fileNameSerializer(prefix, zeros, index, fileType) {
 }
 
 // get the x position of the mouse
-// normalize x position [0.00, 1.00]
-// normalize image
+// normalize x position [0.00, 1.00], xn
+// declare the length of the image sequence, l
+// multiply xn by l, i
+// round i to next highest integer
+// return sequence[i]
